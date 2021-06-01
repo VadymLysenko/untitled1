f4617d9666d8b8deef8b555acebfcb1d427976c7
VadymLysenko <vesioluy@hotmail.com>
2021-06-01 18:44:43 -0500

    added new class StaticMethods

---
+++ src/day_49_static/StaticMethods.java
@@ -0,0 +1,11 @@
+package day_49_static;
+
+public class StaticMethods {
+
+    int num = 10;
+
+    public static void displayMessage(String message) {
+
+        System.out.println("message: " + message);
+    }
+}
