de8780b2be4e6210f10172a5d991a277fbabc63d
VadymLysenko <vesioluy@hotmail.com>
2021-06-05 14:46:01 -0500

    added bookobject class

--- src/day_50_inheritance/BookObjects.java
+++ src/day_50_inheritance/BookObjects.java
@@ -7,6 +7,11 @@ public class BookObjects {
         book.author = "Savitch";
         book.type = "programming";
         book.price = 85.0;
+
+        System.out.println(book.title);
+        System.out.println(book.author);
+        System.out.println(book.type);
+        System.out.println(book.price);
         
     }
 }
