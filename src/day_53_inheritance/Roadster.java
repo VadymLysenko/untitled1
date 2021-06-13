30440ed803f0efb2e4d043fdadb69531eb301d52
VadymLysenko <vesioluy@hotmail.com>
2021-06-13 11:50:42 -0500

    added new subClass ModelX

--- src/day_53_inheritance/Roadster.java
+++ src/day_53_inheritance/Roadster.java
@@ -3,6 +3,7 @@ package day_53_inheritance;
 public class Roadster extends ElectricCar {
     public Roadster(String model, double price, int year, int range) {
         super("Tesla", model, price, year, range);
+        System.out.println("Welcome new Roadster!");
 
     }
 }
