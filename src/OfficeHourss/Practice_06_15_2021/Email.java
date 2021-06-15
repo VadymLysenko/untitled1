c8d5b493d059c58254176eb97479b587db116e7b
VadymLysenko <vesioluy@hotmail.com>
2021-06-15 16:45:56 -0500

    added new package and class Email

---
+++ src/OfficeHourss/Practice_06_15_2021/Email.java
@@ -0,0 +1,14 @@
+package OfficeHourss.Practice_06_15_2021;
+
+public class Email {
+    final String ADDRESS;
+    int numberOfEmail;
+    static String domain;
+
+    static {
+        domain = "generic";
+    }
+    public Email (String address, int numberOfEmail) {
+
+    }
+}
