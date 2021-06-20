e2608088f178ce085f85359b93811b455aff6ff7
VadymLysenko <vesioluy@hotmail.com>
2021-06-20 14:05:27 -0500

    - Flag drawing abstraction practice

--- src/day_55_abstraction/flags/FlagDrawing.java
+++ src/day_55_abstraction/flags/FlagDrawing.java
@@ -6,5 +6,11 @@ public class FlagDrawing {
     public static void main(String[] args) {
         RussianFlag russianFlag = new RussianFlag();
         russianFlag.draw();
+
+        UkrainianFlag ukrainianFlag = new UkrainianFlag();
+        ukrainianFlag.draw();
+
+        FrenchFlag frenchFlag = new FrenchFlag();
+        frenchFlag.draw();
     }
 }
