689cf6948328a0d41df5b5d27d7addff1c9cd6c0
VadymLysenko <vesioluy@hotmail.com>
2021-06-20 15:07:29 -0500

    new sub package interfaces intro

---
+++ src/day_55_abstraction/interface_demo/MyInterface.java
@@ -0,0 +1,22 @@
+package day_55_abstraction.interface_demo;
+
+public interface MyInterface {
+public  abstract void learn();
+}
+interface MyInterface2 {
+
+}
+
+class MyClass implements MyInterface, MyInterface2 {
+
+    @Override
+    public void learn() {
+        System.out.println("Learning in MyClass");
+    }
+}
+
+class Main {
+    public static void main(String[] args) {
+        MyClass myClass = new MyClass();
+    }
+}
