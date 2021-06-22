6a34515cb7a194129051f9b296913c1728125bb3
VadymLysenko <vesioluy@hotmail.com>
2021-06-22 18:28:11 -0500

    day 56 abstraction Greeting

---
+++ src/day_56_abstraction/Greeting.java
@@ -0,0 +1,6 @@
+package day_56_abstraction;
+    public interface Greeting {
+        public abstract void hi();
+
+        public abstract void bye();
+}
---
+++ src/day_56_abstraction/MountainLanguage.java
@@ -0,0 +1,12 @@
+package day_56_abstraction;
+
+public class MountainLanguage implements Greeting{
+@Override
+    public void hi() {
+    System.out.println("Dobruj den");
+}
+@Override
+    public void bye() {
+    System.out.println("Do pobachennia");
+}
+ }
