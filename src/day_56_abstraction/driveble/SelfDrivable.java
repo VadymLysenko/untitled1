485f7e62bcfd07b2f243966e59541287fbc5a43a
VadymLysenko <vesioluy@hotmail.com>
2021-06-22 21:02:18 -0500

    day 56 abstraction

--- src/day_56_abstraction/driveble/SelfDrivable.java
+++ src/day_56_abstraction/driveble/SelfDrivable.java
@@ -2,4 +2,8 @@ package day_56_abstraction.driveble;
 
 public interface SelfDrivable{
     void autoPiloting();
+
+    default void selfPark() {// if we add default method we will prevent errors
+        System.out.println("performing self park steps");
+    }
 }
