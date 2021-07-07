e72da39be09508d07d3382375d65c37b452baa6b
VadymLysenko <vesioluy@hotmail.com>
2021-07-06 19:25:18 -0500

    day 60 exceptions

---
+++ src/day_100_practice_my_own/ReverseString1.java
@@ -0,0 +1,11 @@
+package day_100_practice_my_own;
+
+public class ReverseString1 {
+    public static void main(String[] args) {
+        String input1 = "GeeksForGeeks";
+        char[] try1 = input1.toCharArray();
+
+        for(int i = try1.length - 1; i >= 0; i--)
+            System.out.print(try1[i]);
+    }
+}
---
+++ src/day_100_practice_my_own/ReverseString2.java
@@ -0,0 +1,14 @@
+package day_100_practice_my_own;
+
+public class ReverseString2 {
+    public static void main(String[] args) {
+        // shortest way to reverse string.
+        // String class does not have reverse() method
+        // we need to convert the input string to StringBuffer
+        String str = "Geeks";
+
+        StringBuffer sbr = new StringBuffer(str);
+        sbr.reverse();
+        System.out.println(sbr);
+    }
+}
