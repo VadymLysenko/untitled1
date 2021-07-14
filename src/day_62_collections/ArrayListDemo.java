62b05b7b10121d5cfcf076502a082666c13af50d
VadymLysenko <vesioluy@hotmail.com>
2021-07-13 19:34:47 -0500

    day 62 collections

---
+++ src/day_62_collections/ArrayListDemo.java
@@ -0,0 +1,21 @@
+package day_62_collections;
+
+import java.util.*;
+
+public class ArrayListDemo {
+    public static void main(String[] args) {
+        ArrayList<Integer> nums = new ArrayList<>();
+        List<String> cities = new ArrayList<>(); // polymorphism
+        Collection<String> schools = new ArrayList<>();
+
+        cities.add("New York");
+        cities.add("Vienna");
+        cities.add("Chicago");
+        cities.add("New York");
+
+        System.out.println(cities);
+
+        System.out.println("First city = " + cities.get(0));
+        System.out.println("Count of cities = " + cities.size());
+    }
+}
