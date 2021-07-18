3cd2e14b5e12cfa98d97c9060b91cf0766b034ae
VadymLysenko <vesioluy@hotmail.com>
2021-07-18 11:38:06 -0500

    day 63 collection added class Maps

---
+++ src/day_63_colections/MapsExample.java
@@ -0,0 +1,30 @@
+package day_63_colections;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MapsExample {
+
+    public static void main(String[] args) {
+
+        Map<Integer, String> map = new HashMap<>();
+        map.put(1,"James");
+        map.put(2,"Anna");
+        map.put(3,"Daria");
+        map.put(100,"Maxim");
+
+
+        System.out.println("map = " + map);
+        System.out.println(map.get(100));
+
+        map.remove(1);
+        System.out.println(map);
+        map.put(1,"Kinga");
+        map.remove(2);
+        map.put(2,"Kinga");
+        map.remove(3);
+        map.put(3,"Kinga");
+        map.remove(100);
+        System.out.println(map);
+    }
+}
